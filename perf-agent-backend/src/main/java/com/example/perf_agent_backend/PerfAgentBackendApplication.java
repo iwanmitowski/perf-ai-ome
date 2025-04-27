0f43ec1dd722f10bb9d33ea7c05e29fd41982b04
DESKTOP-C1C2M07\Ivan <iwan.mitowski@gmail.com>
2025-04-27 21:49:23 +0300

    Initial commit

---
+++ perf-agent-backend/src/main/java/com/example/perf_agent_backend/PerfAgentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.perf_agent_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfAgentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfAgentBackendApplication.class, args);
+	}
+
+}
